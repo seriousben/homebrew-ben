7d9daf7befa0963152ad1359cb3bc1e1d051a85f
Benjamin Boudreau <boudreau.benjamin@gmail.com>
2016-06-23 16:50:37 -0400

    Adding playground support for Foko

--- Casks/foko-playground.rb
+++ Casks/foko-playground.rb
@@ -1,8 +1,8 @@
-cask 'foko' do
+cask 'foko-playground' do
   version '4.0.0-2'
   sha256 :no_check
 
-  url 'https://s3.amazonaws.com/foko-packages/beta-Foko.zip'
+  url 'https://s3.amazonaws.com/foko-packages/develop-Foko.zip'
   name 'Foko'
   homepage 'http://foko.co/'
   license :gratis
