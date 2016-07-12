d49aa0f3859f2b8c22bc1d786d5fdbae6ea23dd6
Benjamin Boudreau <boudreau.benjamin@gmail.com>
2016-07-12 13:55:52 -0400

    Update foko.rb

--- Casks/foko.rb
+++ Casks/foko.rb
@@ -2,7 +2,7 @@ cask 'foko' do
   version '4.0.0-2'
   sha256 :no_check
 
-  url 'https://s3.amazonaws.com/foko-packages/beta-Foko.zip'
+  url 'https://s3.amazonaws.com/foko-packages/Foko.zip'
   name 'Foko'
   homepage 'http://foko.co/'
   license :gratis
